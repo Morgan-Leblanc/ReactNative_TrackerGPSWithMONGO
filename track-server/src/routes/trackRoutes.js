53228aafd0657c535505be3bfd100ea7f0223e41
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-03-13 16:27:35 +0100

    modif url ngrok

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const requireAuth = require('../middlewares/requireAuth');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middlewares/requireAuth");
 
-const Track = mongoose.model('Track');
+const Track = mongoose.model("Track");
 
 const router = express.Router();
 
 router.use(requireAuth);
 
-router.get('/tracks', async (req, res) => {
+router.get("/tracks", async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id });
-
+  
   res.send(tracks);
 });
 
-router.post('/tracks', async (req, res) => {
+router.post("/tracks", async (req, res) => {
   const { name, locations } = req.body;
 
   if (!name || !locations) {
     return res
       .status(422)
-      .send({ error: 'You must provide a name and locations' });
+      .send({ error: "You must provide a name and locations" });
   }
 
   try {
